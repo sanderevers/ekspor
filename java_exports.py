f78a39f6fc59d734278f8f004d1a9349a4eeef8b
Sander Evers <sander.evers@topicus.nl>
2015-01-22 15:04:09 +0100

    initial commit FOR REAL

---
+++ java_exports.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python
+
+import subprocess
+import json
+
+
+def fetch_filenames():
+    SRC_DIR = '/Users/sandr/work/eduarte'
+    SRC_REGEX = '.*/src/main/java/.*\.java$'
+    return subprocess.check_output(['find',SRC_DIR,'-regex',SRC_REGEX]).splitlines()
+
+def class_from_filename(filename):
+    with_slashes = filename.split('/src/main/java/')[-1]
+    without_extension = with_slashes[0:with_slashes.rindex('.'):]
+    with_dots = without_extension.replace('/','.')
+    return with_dots
+
+# TODO support wildcard imports / implicit package imports
+def classes_from_file(text):
+    imports = filter(lambda s: s.startswith('import '), text.splitlines())
+    classes = [c[len('import '):-1] for c in imports]
+    return set(classes)
+
+# a TREE consists of:
+# - a set of BRANCHES (which are trees themselves) named by tuples of strings
+#   whose (tuple) prefixes do not overlap
+# - a LEAF (optional if there are branches): empty tuple
+# - a dict string -> arbitrary value
+
+def branches(t):
+    return dict((k,v) for (k,v) in t.iteritems() if type(k) is tuple and len(k)>0)
+
+NoLeaf = object()
+def leaf(t):
+    try:
+        return t[()]
+    except KeyError:
+        return NoLeaf
+
+def anns(t):
+    return dict((k,v) for (k,v) in t.iteritems() if not type(k) is tuple)
+
+def tree(branches,leaf=NoLeaf,anns={}):
+    ret = {}
+    assert all(type(k) is tuple and len(k)>0 for k in branches.keys())
+    ret.update(branches)
+    assert all(not type(k) is tuple for k in anns.keys())
+    ret.update(anns)
+    if not leaf is NoLeaf:
+        ret[()] = leaf
+    return ret
+
+def hasleaf(t):
+    return () in t
+
+def treeify_simple(flat):
+    tree = {}
+    for k,v in flat.iteritems():
+        if k: # skip empty tuple here
+            firstlettermap = tree[k[0]] = tree.get(k[0],{})
+            firstlettermap[tuple(k[1:])] = v
+    ttree={}
+    for k,firstlettermap in tree.iteritems():
+        ttree[tuple([k])] = treeify_simple(firstlettermap)
+    if () in flat:
+        ttree[()] = flat[()]
+    return ttree
+
+def ann_size(tree):
+    cumsize = 0
+    for k,v in tree.iteritems():
+        if k==():
+            cumsize += len(v)
+        elif type(k) is tuple:
+            ann_size(v)
+            cumsize += v['size']
+    tree['size'] = cumsize
+    if () in tree:
+        tree['$size'] = len(tree[()])
+    return tree
+
+def filter_smallsize(t,maxsize):
+    newbr = dict((k,v) for (k,v) in branches(t).iteritems() if anns(v)['size']<maxsize)
+    newanns = dict(anns(t))
+    newanns['size']=sum(anns(v)['size'] for (k,v) in newbr.iteritems())
+    return tree(newbr,leaf(t),newanns)
+
+
+def collect_smallsize(t, minsize):
+    branchname = ('<'+str(minsize),)
+    newbr = {}
+    for k,v in branches(t).iteritems():
+        branchsize = anns(v)['size']
+        if branchsize < minsize:
+            newbr[branchname]=newbr.get(branchname,tree({},anns={'size':0}))
+            newbr[branchname]['size'] += branchsize
+        else:
+            newbr[k] = collect_smallsize(v,minsize)
+    return tree(newbr,leaf(t),anns(t))
+
+
+def collapse_minsize(tree, minsize):
+    ctree = {}
+    for k,v in tree.iteritems():
+        if k==():
+            ctree[k] = v
+        elif type(k) is str:
+            ctree[k] = v
+        elif v['size']<minsize:
+            ctree[k] = {():None, '$size':v['size']}
+        else:
+            ctree[k] = collapse_minsize(v, minsize)
+    return ctree
+
+def concat_paths(t):
+    def update_branch(k,v):
+        branches_v = branches(v)
+        if len(branches_v)==1 and not(hasleaf(v)):
+            branchname = branches_v.keys()[0]
+            subtree = branches_v.values()[0]
+            return k+branchname,concat_paths(subtree)
+        else:
+            return k,concat_paths(v)
+    updated_branches = dict(update_branch(k,v) for (k,v) in branches(t).iteritems())
+    return tree(updated_branches, leaf(t), anns(t))
+
+'''
+def aggregate_paths(t):
+    br = branches(t)
+    if len(br)==1 and not(hasleaf(t)):
+        newbr = {}
+        prefix = br.keys()[0]
+        subt = br.values()[0]
+        for k,v in branches(subt).iteritems():
+            newbr[prefix+k] = aggregate_paths(v)
+        newanns = {}
+        newanns.update(anns(t))
+        newanns.update(anns(subt))
+        return tree(newbr,leaf(subt),newanns)
+    else:
+        newbr = {}
+        for k,v in br.iteritems():
+            newbr[k] = aggregate_paths(v)
+        return tree(newbr, leaf(t), anns(t))
+
+
+
+def aggregate_paths(tree):
+    atree = {}
+    if len(tree)==1 and tree.keys()[0] != ():
+        prefix = tree.keys()[0]
+        for k,v in tree.values()[0].iteritems():
+            if k==() or type(k) is str:
+                atree[k] = v
+            else:
+                atree[tuple(list(prefix)+list(k))] = aggregate_paths(v)
+    else:
+        for k,v in tree.iteritems():
+            if k==() or type(k) is str:
+                atree[k] = v
+            else:
+                atree[k] = aggregate_paths(v)
+    return atree
+'''
+
+def lookup(path, t):
+    if len(path)==0:
+        return t
+    br = branches(t)
+    for i in range(1,len(path)+1):
+        if path[:i] in br:
+            return lookup(path[i:],br[path[:i]])
+    raise KeyError()
+
+
+
+def common_prefix(*strings):
+    prefix = strings[0]
+    for s in strings[1:]:
+#        if not s.startswith(prefix):  # for performance
+            prefix = tuple(iter_prefix(prefix,s))
+    return prefix
+
+
+def iter_prefix(s1,s2):
+    for c1,c2 in zip(s1,s2):
+        if c1==c2:
+            yield c1
+        else:
+            return
+
+def to_treemap(tree,rootpath=()):
+    if len(branches(tree))==1:
+        return _to_treemap(tree,rootpath)[0]
+    else:
+        return {'name':'^', 'path':'.'.join(rootpath), 'children':_to_treemap(tree,rootpath)}
+
+def _to_treemap(tree,stack):
+    branches = []
+    for k,v in tree.iteritems():
+        if type(k) is str:
+            pass
+        elif k==():
+            branches.append({'name':'$', 'size':tree['$size']})
+        else:
+            children = _to_treemap(v,stack+k)
+            branches.append({'name':'.'.join(k), 'path':'.'.join(stack+k), 'children': children})
+    return branches
+
+def read_flat():
+    filenames = fetch_filenames()
+    imports = {}
+    exports = {}
+    for filename in filenames:
+        with open(filename) as f:
+            clazz = class_from_filename(filename)
+            if clazz in imports:
+                print 'WARNING: duplicate class '+clazz
+            imports[clazz] = classes_from_file(f.read())
+            exports[clazz] = set()
+    for clazz, imp_classes in imports.items():
+        for imp_class in imp_classes:
+            try:
+                exports[imp_class].add(clazz)
+            except KeyError:
+                pass
+
+    exports_tup = {}
+    for name,ex in exports.iteritems():
+        exports_tup[tuple(name.split('.'))] = ex
+
+    return exports_tup
+
+def construct_annotated_tree():
+    exports_tup = read_flat()
+    tree = treeify_simple(exports_tup)
+    ctree = concat_paths(tree)
+    atree = ann_size(ctree)
+    return atree
+
+
+def main():
+    atree = construct_annotated_tree()
+    ctree = collapse_minsize(atree,atree['size']/40)
+    treemap = to_treemap(ctree)
+
+    with open('exports.json','w') as f:
+        f.write(json.dumps(treemap,indent=2))
+
+    return atree, ctree
---
+++ webserver.py
@@ -0,0 +1,96 @@
+# import string,cgi,time
+# from os import curdir, sep
+import java_exports
+import json
+import time
+import urllib
+import urlparse
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+HOST = 'localhost'
+PORT = 8888
+BASE_URL = 'http://{host}:{port}'.format(host=HOST,port=PORT)
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            starttime = time.time()
+            urlparts = urlparse.urlparse(self.path)
+            clz = tuple(c for c in urllib.unquote(urlparts.path).replace('/','.').split('.') if c)
+            query = urlparse.parse_qs(urllib.unquote(urlparts.query))
+            view = query.get('view',[None])[0]
+            collectsize = query.get('collectsize',[None])[0]
+            content_type = 'text/html' if view=='html' else 'application/json'
+            print('clz={clz}'.format(clz=clz))
+            print('query={query}'.format(query=query))
+            try:
+                if len(clz)>0 and clz[-1].startswith("<"):
+                    realclz = clz[:-1]
+                    maxsize = clz[-1][1:]
+                    subtree = java_exports.filter_smallsize(java_exports.lookup(realclz, MyHandler.tree),maxsize)
+                else:
+                    realclz = clz
+                    subtree = java_exports.lookup(realclz, MyHandler.tree)
+                    if collectsize:
+                        if collectsize[-1]=='%':
+                            smallsize = int(subtree['size'] * float(collectsize[:-1])/100)
+                        else:
+                            smallsize = int(collectsize)
+                        subtree = java_exports.collect_smallsize(subtree,smallsize)
+                self.send_response(200)
+                self.send_header('Content-type',content_type)
+                self.send_header('Access-Control-Allow-Origin','*')
+                self.end_headers()
+
+                if content_type=='application/json':
+                    self.wfile.write(json.dumps(java_exports.to_treemap(subtree,realclz),indent=2))
+                else:
+                    self.wfile.write(MyHandler.tree_to_html(realclz,subtree))
+            except KeyError:
+                self.send_error(404)
+            print('  {:.0f} ms'.format((time.time()-starttime)*1000))
+
+            return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    @staticmethod
+    def tree_to_html(clz,tree):
+        items = ['<li><a href="{docroot}/{clzpath}.{key}?view=html">{key}</a></li>'.format(docroot=BASE_URL,clzpath='.'.join(clz),key='.'.join(k))
+                    for k in tree.keys() if type(k) is tuple]
+        exports = ['<li>{clz}</li>'.format(clz=clz) for clz in tree.get((),[])]
+        attrs = {k:v for k,v in tree.iteritems() if type(k) is str}
+        return '''
+        <html><body>
+        <div>{attr_json}</div>
+        <div>Exports:<ul>
+        {exports}
+        </div></ul>
+        <div>Children:<ul>
+            {items}
+        </ul></div>
+        </body>
+        </html>
+        '''.format(
+            items='\n'.join(items),
+            attr_json = json.dumps(attrs,indent=2),
+            exports = '\n'.join(exports) )
+
+
+
+
+
+def main():
+    try:
+        MyHandler.tree = java_exports.construct_annotated_tree()
+        server = HTTPServer(('', PORT), MyHandler)
+        print 'serving HTTP on port {port}...'.format(port=PORT)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
