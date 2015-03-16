52bc304fada53683595c6291299ad2a05c52656b
Sander Evers <sander.evers@topicus.nl>
2015-03-16 08:59:24 +0100

    frontend: use /etc/hosts + static port instead of sed substitution

--- frontend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# substitute fake variable $backend
-sed -e "s/\$backend/$BACKEND_PORT_8888_TCP_ADDR:$BACKEND_PORT_8888_TCP_PORT/" -i "" /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
